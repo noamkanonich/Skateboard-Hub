cd9c2bb7e71ffdd3b3a51eb142a9012c1c165bbe
Noam Kanonich <55742997+noamkanonich@users.noreply.github.com>
2020-07-29 08:25:16 +0300

    first commit

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,34 +1,41 @@
-
-  (function($) {
+(function ($) {
   "use strict"; // Start of use strict
 
   // Smooth scrolling using jQuery easing
-  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
-    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
+    if (
+      location.pathname.replace(/^\//, "") ==
+      this.pathname.replace(/^\//, "") &&
+      location.hostname == this.hostname
+    ) {
       var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      target = target.length ? target : $("[name=" + this.hash.slice(1) + "]");
       if (target.length) {
-        $('html, body').animate({
-          scrollTop: (target.offset().top - 72)
-        }, 1000, "easeInOutExpo");
+        $("html, body").animate(
+          {
+            scrollTop: target.offset().top - 72,
+          },
+          1000,
+          "easeInOutExpo"
+        );
         return false;
       }
     }
   });
 
   // Closes responsive menu when a scroll trigger link is clicked
-  $('.js-scroll-trigger').click(function() {
-    $('.navbar-collapse').collapse('hide');
+  $(".js-scroll-trigger").click(function () {
+    $(".navbar-collapse").collapse("hide");
   });
 
   // Activate scrollspy to add active class to navbar items on scroll
-  $('body').scrollspy({
-    target: '#mainNav',
-    offset: 75
+  $("body").scrollspy({
+    target: "#mainNav",
+    offset: 75,
   });
 
   // Collapse Navbar
-  var navbarCollapse = function() {
+  var navbarCollapse = function () {
     if ($("#mainNav").offset().top > 100) {
       $("#mainNav").addClass("navbar-scrolled");
     } else {
@@ -41,159 +48,91 @@
   $(window).scroll(navbarCollapse);
 
   // Magnific popup calls
-  $('#gallery').magnificPopup({
-    delegate: 'a',
-    type: 'image',
-    tLoading: 'Loading image #%curr%...',
-    mainClass: 'mfp-img-mobile',
+  $("#gallery").magnificPopup({
+    delegate: "a",
+    type: "image",
+    tLoading: "Loading image #%curr%...",
+    mainClass: "mfp-img-mobile",
     gallery: {
       enabled: true,
       navigateByImgClick: true,
-      preload: [0, 1]
+      preload: [0, 1],
     },
     image: {
-      tError: '<a href="%url%">The image #%curr%</a> could not be loaded.'
-    }
+      tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+    },
   });
 
   // add item to cart
-  var cart_list = []
+  var cart_list = [];
   var total_money = 0;
-  $('.product-item').click(function() {
+  $(".product-item").click(function () {
     var product_name = this.id;
-    console.log("AAAAAAAAAAAAAAAAA")
-    // var price = this.val();
-    // alert(product_name);
 
-    if(cart_list.includes(product_name)){
+    if (cart_list.includes(product_name)) {
       return;
-    }
-    else {
+    } else {
       cart_list.push(product_name);
       total_money = total_money + parseInt(price);
     }
-  })
-  
-
-
-
-  // $('.cart-list').click(function() {
-   
-  //     this.innerHTML = hide ? 'Show Less' : 'Show More'
-  // })
+  });
 
   var i = 1;
   var list = document.getElementById("product-list");
 
   // Show cart list
-  $('.show-cart').click(function() {
-    cart_list.forEach(product => {
-      list.innerHTML += '<li>'+product + i++ + '</li>'
-      
-      
-    })
-  })
-
+  $(".show-cart").click(function () {
+    cart_list.forEach((product) => {
+      list.innerHTML += "<li>" + product + i++ + "</li>";
+    });
+  });
 
   // ADDING TO CART
   var i = 1;
   var list = document.getElementById("product-list");
-  var add = document.getElementById('addProduct');
-  var remove = document.getElementById('removeProduct');
-  var product = document.getElementById('')
+  var add = document.getElementById("addProduct");
+  var remove = document.getElementById("removeProduct");
+  var product = document.getElementById("");
 
-  add.addEventListener('click', function() {
+  add.addEventListener("click", function () {
     var itemsByTagName = document.getElementsByTagName("li");
-    list.innerHTML += '<li>item ' + i++ + '</li>'
+    list.innerHTML += "<li>item " + i++ + "</li>";
   });
 
-  remove.removeEventListener('click', function() {
-      var itemsByTagName = document.getElementsByTagName("li");
-      list.innerHTML += "<p></p>"
-    });
-
-
- 
+  remove.removeEventListener("click", function () {
+    var itemsByTagName = document.getElementsByTagName("li");
+    list.innerHTML += "<p></p>";
+  });
 })(jQuery);
 
-
 var clicked = false;
-$(".checkall").on("click", function() {
+$(".checkall").on("click", function () {
   $(".checkhour").prop("checked", !clicked);
   clicked = !clicked;
-  this.innerHTML = clicked ? 'Deselect' : 'Select';
+  this.innerHTML = clicked ? "Deselect" : "Select";
 });
 
-
-// $('.load-more').click(function(){
-//   alert("AA")
-//   document.getElementById("more").addClass("show-more");
-// })
-
-// function show(){
-//   if(document.getElementById("more").className === "show-less"){
-//     document.getElementById("more").className =+ "show-more";
-//  }
-//   else{
-//     document.getElementById("more").className = "show-less";
-//   }
-// }
-
-
-  
 var hide = false;
-function toggleProducts(){
+function toggleProducts() {
   var x = document.getElementById("more");
   if (x.style.display === "none") {
     x.style.display = "block";
     x.addClass += "row";
     hide = false;
     document.getElementById("showmore").innerHTML = "Show Less";
-
   } else {
     x.style.display = "none";
-    hide = true
+    hide = true;
     document.getElementById("showmore").innerHTML = "Show More";
   }
 }
 
-
 // check if there is any checked input in form
 function check() {
-    if ($("#cart-form input:checkbox:checked").length > 0)
-    {
-        alert("GOOD")
-        document.getElementById('cart-form').submit();
-    }
-    else
-    {
-      alert("")
-    }
+  if ($("#cart-form input:checkbox:checked").length > 0) {
+    alert("GOOD");
+    document.getElementById("cart-form").submit();
+  } else {
+    alert("");
+  }
 }
-
-
-//  keep checkboxes checked
-// function saveProduct1(){
-//   var checkbox1 = document.getElementById('product1check');
-//   localStorage.setItem('product1check', checkbox1.checked);
-// }
-
-// function load(){    
-//   var checked1 = JSON.parse(localStorage.getItem('product1check'));
-//   document.getElementById("product1check").checked = checked1;
-// }
-
-
-
-// function validateSitec(){
-//   if (document.getElementById('sitecheck').checked){
-//       $('#products').prop('checked', true);
-//       $.cookie("cookieChecked", "#products");
-//   }else if(document.getElementById('closedmsg').checked){
-//       $('#closedmsg').slideUp();
-//       $("#sitecheck").removeProp("checked").checkboxradio("refresh");
-//       $.cookie("cookieChecked", "#closedmsg");
-//   } else {
-//       $.cookie("cookieChecked","");
-//   }
-//   }
